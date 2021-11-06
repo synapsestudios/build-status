001258056757b9d6a1d0f6b2606c55d53520eb62
chestercharles <chestercarmer@icloud.com>
2021-11-06 16:30:21 -0700

    fix private class variables

--- src/SlackMessage.js
+++ src/SlackMessage.js
@@ -28,15 +28,15 @@ class SlackMessage extends SlackMessageRoot {
   constructor(token, message = { channel: '', ts: '' }) {
     super();
     if (!token) throw new Error('token is required');
-    this._token = token;
-    this._channel = message.channel;
-    this._ts = message.ts;
+    this.#_token = token;
+    this.#_channel = message.channel;
+    this.#_ts = message.ts;
   }
 
   async _postRequest(method, body) {
     const response = await got.post(`https://slack.com/api/${method}`, {
       headers: {
-        Authorization: `Bearer ${this._token}`,
+        Authorization: `Bearer ${this.#_token}`,
       },
       json: body,
       responseType: 'json',
@@ -47,27 +47,27 @@ class SlackMessage extends SlackMessageRoot {
   }
 
   async initialize(message) {
-    if (!this._channel) throw new Error('channel is required');
+    if (!this.#_channel) throw new Error('channel is required');
     const response = await this._postRequest('chat.postMessage', {
       ...message,
-      channel: this._channel,
+      channel: this.#_channel,
     });
-    this._ts = response.message.ts;
+    this.#_ts = response.message.ts;
   }
 
   async sendBlock(block) {
-    if (!this._channel) throw new Error('Channel is required');
-    if (!this._ts) throw new Error('ts is required');
+    if (!this.#_channel) throw new Error('Channel is required');
+    if (!this.#_ts) throw new Error('ts is required');
 
     const historySearchParams = new URLSearchParams();
-    historySearchParams.append('channel', this._channel);
-    historySearchParams.append('latest', this._ts);
+    historySearchParams.append('channel', this.#_channel);
+    historySearchParams.append('latest', this.#_ts);
     historySearchParams.append('limit', 1);
     historySearchParams.append('inclusive', 'true');
 
     const response = await got.get(`https://slack.com/api/conversations.history?${historySearchParams.toString()}`, {
       headers: {
-        Authorization: `Bearer ${this._token}`,
+        Authorization: `Bearer ${this.#_token}`,
       },
       responseType: 'json',
     });
@@ -77,13 +77,13 @@ class SlackMessage extends SlackMessageRoot {
 
       await this._postRequest('chat.update', {
         text: message.text,
-        channel: this._channel,
-        ts: this._ts,
+        channel: this.#_channel,
+        ts: this.#_ts,
         blocks: overwriteOrAppendBlock(message.blocks),
       });
     } else {
       await this._postRequest('chat.postMessage', {
-        channel: this._channel,
+        channel: this.#_channel,
         blocks: [
           {
             type: "section",
@@ -109,7 +109,7 @@ class SlackMessage extends SlackMessageRoot {
   }
 
   get ts() {
-    return this._ts;
+    return this.#_ts;
   }
 }
 
