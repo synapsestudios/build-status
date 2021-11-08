8f254809cbf38018dddf148469cd4f77577278ae
chestercharles <chestercarmer@icloud.com>
2021-11-08 12:06:54 -0700

    Move action inputs/context into a function

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const { SlackMessage } = require("./src/SlackMessage");
 const initializeMessage = require("./src/initializeMessage");
 const reportJobStatus = require("./src/reportJobStatus");
+const getActionParams = require("./src/getActionParams");
 
 const execute = async function () {
-  const octokit = github.getOctokit(core.getInput("githubToken"));
-  const run = await octokit.rest.actions.getWorkflowRun({
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    run_id: github.context.runId,
+  const params = await getActionParams();
+
+  const slackMessage = new SlackMessage(params.token, {
+    channel: params.channel,
+    ts: params.messageTs,
   });
 
-  const slackMessage = new SlackMessage(core.getInput("token"), {
-    channel: core.getInput("channel"),
-    ts: core.getInput("messageTs"),
-  });
-
-  if (core.getInput("type") === "trigger") {
-    const sender = github.context.payload.sender;
+  if (params.type === "trigger") {
     const messageTs = await initializeMessage(slackMessage)(
-      { name: sender.login, avatar: sender.avatar_url },
-      run.data.html_url,
-      core.getInput("header") || undefined
+      { name: params.senderName, avatar: params.senderAvatar },
+      params.runUrl,
+      params.header
     );
     core.setOutput("messageTs", messageTs);
   } else {
     await reportJobStatus(slackMessage)({
-      runId: github.context.runId,
-      job: core.getInput("name") || github.context.job,
+      job: params.job,
+      runId: params.runId,
       status: "in_progress",
     });
   }
