869514637df183cbab9f5dda8f9f3c731b8687c5
spruce-bruce <aaron@synapsestudios.com>
2022-04-05 10:13:49 -0700

    Adds a test for the perms error codepath
    Also fixes it to actually work :(

--- test/SlackMessage.spec.js
+++ test/SlackMessage.spec.js
@@ -151,4 +151,42 @@ describe("SlackMessage", () => {
       },
     });
   });
+
+  it('Sends a message to slack channel when bot does not have access to channel history', async () => {
+    const message = new SlackMessage("TOKEN", {
+      channel: "C12345",
+      ts: "1234.1234",
+    });
+
+    server.use(
+      rest.get(
+        "https://slack.com/api/conversations.history",
+        (_req, res, ctx) => {
+          return res(ctx.status(422));
+        }
+      )
+    );
+
+    server.use(
+      rest.post("https://slack.com/api/chat.postMessage", (_req, res, ctx) => {
+        return res(ctx.json(mockMessageResponse()));
+      })
+    );
+
+    const postEvents = waitForRequest(
+      server,
+      "POST",
+      "https://slack.com/api/chat.postMessage"
+    );
+
+    await message.sendBlock({
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: ":grey_exclamation:   api starting build",
+      },
+    });
+
+    await postEvents;
+  });
 });
