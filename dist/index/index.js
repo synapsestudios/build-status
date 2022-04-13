a55de3efdad1d4c877fa081868b1043c72a652d1
spruce-bruce <aaron@synapsestudios.com>
2022-04-13 13:23:48 -0700

    [Automated Commit] Builds recent changes

--- dist/index/index.js
+++ dist/index/index.js
@@ -16635,7 +16635,6 @@ const useCaseMap = {
     });
   },
   link: async (slackMessage, params) => {
-    console.log(params);
     await appendHeaderLink(slackMessage)({
       url: params.link,
       text: params.link_text,
