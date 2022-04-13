471bb7f6b25bf39cfeb9b9f6fa3c705bb1fc05a9
spruce-bruce <aaron@synapsestudios.com>
2022-04-13 13:23:48 -0700

    removes an errant console.log

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const useCaseMap = {
     });
   },
   link: async (slackMessage, params) => {
-    console.log(params);
     await appendHeaderLink(slackMessage)({
       url: params.link,
       text: params.link_text,
